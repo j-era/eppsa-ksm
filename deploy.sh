f499634b4929ad1912c8bcdd2794c028286da044
Jens Eraßmy <jens.erassmy@artcom.de>
2018-03-02 16:53:20 +0100

    Update deploy script
    Trigger new build of the images

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ git clone https://github.com/artcom/eppsa-ksm.git
 cd eppsa-ksm
 docker-compose stop
 docker-compose rm -f
+docker-compose -f docker-compose.yml -f docker-compose.production.yml build
 docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
 EOF
