13231b01e00086694efdc68af176a101ded67223
Jens Eraßmy <jens.erassmy@artcom.de>
2018-03-02 16:36:41 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ ssh -p $SSH_PORT $SSH_USERNAME@$SSH_HOST -o StrictHostKeyChecking=no <<-EOF
 rm -rf eppsa-ksm
 git clone https://github.com/artcom/eppsa-ksm.git
 cd eppsa-ksm
-docker-compose -f docker-compose.yml -f docker-compose.production.yml up
+docker-compose stop
+docker-compose rm -f
+docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
 EOF
