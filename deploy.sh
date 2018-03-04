93aaac4ccb98a4bce67a95c5ae5f76eaac8350f3
Jens Eraßmy <jens.erassmy@artcom.de>
2018-03-04 11:16:14 +0100

    Update deloy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ssh -p $SSH_PORT $SSH_USERNAME@$SSH_HOST BRANCH_NAME=${BRANCH_NAME}
 ssh -p $SSH_PORT $SSH_USERNAME@$SSH_HOST << EOF
+echo "$BRANCH_NAME"
 rm -rf eppsa-ksm
-git clone --recursive -b ${BRANCH_NAME} https://github.com/artcom/eppsa-ksm.git
+git clone --recursive -b "$BRANCH_NAME" https://github.com/artcom/eppsa-ksm.git
 cd eppsa-ksm
 docker-compose stop
 docker-compose rm -f
