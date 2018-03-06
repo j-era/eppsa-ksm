2d15f17091f5e2eeb3b0c9204b45dedef174ec95
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-03-06 16:44:21 +0100

    Update Jenkins deployment

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,14 +1,13 @@
 pipeline {
   agent any
   environment {
-    SSH_PRIVATE_KEY = credentials('ssh-private-key')
-    SSH_HOST2 = credentials(ssh-host2)
+    SSH_USERNAME = credentials('ssh-username-test')
   }
   stages {
     stage('Deploy') {
       steps {
           node('OSX') {
-             sh '''echo $SSH_HOST2 && ./deploy.sh'''
+             sh '''./deploy.sh'''
           }
       }
     }
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo $SSH_USERNAME
-ssh -p $SSH_PORT $SSH_USERNAME@$SSH_HOST << EOF
-rm -rf eppsa-ksm
-git clone --recursive https://github.com/artcom/eppsa-ksm.git
-cd eppsa-ksm
-git checkout -b $GIT_BRANCH
-docker-compose stop
-docker-compose rm -f
-docker-compose -f docker-compose.yml -f docker-compose.production.yml build
-docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
-EOF
+# ssh -p $SSH_PORT -i $SSH_PRIVATE_KEY $SSH_USERNAME@$SSH_HOST << EOF
+# rm -rf eppsa-ksm
+# git clone --recursive https://github.com/artcom/eppsa-ksm.git
+# cd eppsa-ksm
+# git checkout -b $GIT_BRANCH
+# docker-compose stop
+# docker-compose rm -f
+# docker-compose -f docker-compose.yml -f docker-compose.production.yml build
+# docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
+# EOF
