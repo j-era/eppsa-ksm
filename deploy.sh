2ac2036e1e8aabc61dbf5f8257f0b7701a178bb9
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-03-12 16:34:18 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-ssh -p $SSH_PORT $SSH_USER@$SSH_HOST << EOF
-rm -rf eppsa-ksm
-git clone --recursive https://github.com/artcom/eppsa-ksm.git
-cd eppsa-ksm
-git checkout -b $GIT_BRANCH
-docker-compose stop
-docker-compose rm -f
-docker-compose -f docker-compose.yml -f docker-compose.production.yml build
-docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
+ssh -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_USER@$SSH_HOST << EOF
+  rm -rf eppsa-ksm
+  git clone --recursive https://github.com/artcom/eppsa-ksm.git
+  cd eppsa-ksm
+  git checkout $GIT_BRANCH
+  sudo systemctl stop eppsa-ksm
+  docker-compose rm -f
+  docker-compose -f docker-compose.yml -f docker-compose.production.yml build
+  sudo systemctl start eppsa-ksm
 EOF
