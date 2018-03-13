186e17b61299489c1a9231cdd1e5b765a7cc4a77
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-03-13 13:45:40 +0100

    Use new branch option for git checkout

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ ssh -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_USER@$SSH_HOST << EOF
   rm -rf eppsa-ksm
   git clone --recursive https://github.com/artcom/eppsa-ksm.git
   cd eppsa-ksm
-  git checkout $GIT_BRANCH
+  git checkout -b $GIT_BRANCH
   sudo systemctl stop eppsa-ksm
   docker-compose rm -f
   docker-compose -f docker-compose.yml -f docker-compose.production.yml build
