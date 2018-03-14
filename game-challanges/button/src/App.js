bcc9d049ea31435b6dd4ce8f58e473f0e2890b27
Marco <marco.mojica@artcom.de>
2018-03-14 17:18:14 +0100

    Change out of box app

--- game-challanges/button/src/App.js
+++ game-challanges/button/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
+
+const Container = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  width: 100%;
+  height: 100%;
+`
+const Button = styled.div `
+  
+  width: 100px;
+  height: 100px;
+  background-color: green;
+`
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Button/>
+      </Container>
     );
   }
 }
