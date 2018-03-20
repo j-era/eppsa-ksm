9dd0ff50df3234aa9b49a21eea07bf1a5b398c87
Marco <marco.mojica@artcom.de>
2018-03-20 11:38:07 +0100

    Implement game end

--- game-client/src/components/application.js
+++ game-client/src/components/application.js
@@ -6,34 +6,49 @@ import Game from "./game"
 import StartDialog from "./startDialog"
 
 function Application(props) {
-    const challengeTypes = props.content.challenges[props.challenge].challengeTypes
-    const challengeType = Object.keys(omit(challengeTypes, "template"))[0]
-    const challengeConfig = challengeTypes[challengeType]
-    const challengeUri = resolveChallengeWebAppUri(challengeType)
+
+    const maxChallenge = Object.keys(props.content.challenges).length - 1
+    const gameDone = props.challenge > maxChallenge
+
+  let challengeTypes = null
+  let challengeType = null
+  let challengeConfig = null
+  let challengeUri = null
+
+  if(!gameDone){
+    challengeTypes = props.content.challenges[props.challenge].challengeTypes
+    challengeType = Object.keys(omit(challengeTypes, "template"))[0]
+    challengeConfig = challengeTypes[challengeType]
+    challengeUri = resolveChallengeWebAppUri(challengeType)
+  }
+
 
     return (
       <div>
         <div>{ props.content.description }</div>
-        { props.gameStarted ?
-          <Game
-            challenge={ props.challenge }
-            challengeUri={ challengeUri }
-            challengeConfig={ challengeConfig }
-            score={ props.score }
-            challengeStarted={ props.challengeStarted }
-            onStartChallenge={ props.onStartChallenge }
-            onChallengeReady={ props.onChallengeReady }
-          /> :
-          <StartDialog
-            previousGameInfo={ props.previousGameInfo }
-            name={ props.name }
-            avatars={ props.content.avatars }
-            avatar={ props.avatar }
-            assetServerUri={ props.assetServerUri }
-            onResumeGame={ props.onResumeGame }
-            onStartNewGame={ props.onStartNewGame }
-            onUpdateName={ props.onUpdateName }
-        /> }
+        { gameDone ? <div>Game is Done</div> :
+          ( props.gameStarted ?
+            <Game
+              challenge={ props.challenge }
+              challengeUri={ challengeUri }
+              challengeConfig={ challengeConfig }
+              score={ props.score }
+              challengeStarted={ props.challengeStarted }
+              onStartChallenge={ props.onStartChallenge }
+              onChallengeReady={ props.onChallengeReady }
+            /> :
+            <StartDialog
+              previousGameInfo={ props.previousGameInfo }
+              name={ props.name }
+              avatars={ props.content.avatars }
+              avatar={ props.avatar }
+              assetServerUri={ props.assetServerUri }
+              onResumeGame={ props.onResumeGame }
+              onStartNewGame={ props.onStartNewGame }
+              onUpdateName={ props.onUpdateName }
+            />
+          )
+        }
       </div>
     )
 }
