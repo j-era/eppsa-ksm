cb9fbc326a4d5c5958302f1001b7de463b98ffdb
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-03-23 12:52:09 +0100

    Add selection mechanism

--- game-challenge/quiz/src/App.js
+++ game-challenge/quiz/src/App.js
@@ -15,15 +15,43 @@ const Button = styled.div `
   margin-top: 5px;
   width: 200px;
   height: 100px;
+`
+
+const AnswerButton = styled(Button)`
+  background-color: ${props => props.selected ? "red" : "grey"};
+`
+
+const ConfirmButton = styled(Button)`
   background-color: green;
 `
 
-export default props =>
-  <Container>
-    {
-      props.config.answers.map((answer, i) =>
-        <Button key={ i }>{ answer.answer }</Button>
-      )
-    }
-    <Button onClick={ props.onClick }>Confirm</Button>
-  </Container>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { selected: null }
+  }
+
+  render() {
+    return (
+      <Container>
+        {
+          this.props.config.answers.map((answer, i) =>
+            <AnswerButton
+              key={ i }
+              onClick={ () => this.select(i) }
+              selected={ this.state.selected === i }>
+              {
+                answer.answer
+              }
+            </AnswerButton>
+          )
+        }
+        <ConfirmButton>Confirm</ConfirmButton>
+      </Container>
+    )
+  }
+
+  select(n) {
+    this.setState({ selected: this.state.selected === n ? null : n })
+  }
+}
