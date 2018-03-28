b425766c69b64db475edf1369d1298b7f29e7b9b
Marco <marco.mojica@artcom.de>
2018-03-28 11:33:02 +0200

    Add qr reader to button challenge

--- game-challenge/button/src/App.js
+++ game-challenge/button/src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import styled from "styled-components"
+import QrReader from "react-qr-reader"
 
 const Container = styled.div `
   display: flex;
   align-items: center;
   justify-content: center;
   
+  flex-direction: column;
+  
   width: 100%;
   height: 100%;
 `
-const Button = styled.div `
+const ButtonDiv = styled.div `
   
   width: 100px;
   height: 100px;
   background-color: green;
 `
 
-export default props =>
-  <Container>
-    <Button onClick={ props.onClick }/>
-  </Container>
+class Button extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      scanned: "nothing scanned yet",
+      onClick: props.onClick
+    }
+  }
+
+  render(){
+    return(
+      <Container>
+        <QrReader
+          onScan={ data => this.onScan(data) }
+          onError={ err => this.onError(err) }
+          style={{ width: '50%' }}
+        />
+        <div>{ this.state.scanned }</div>
+        <ButtonDiv onClick={ this.state.onClick }/>
+      </Container>
+    )
+  }
+
+  onScan(data){
+    if(data){
+      this.setState({
+        scanned: data
+      })
+      console.log(data)
+    }
+  }
+
+  onError(err){
+    console.error(err)
+  }
+}
+
+export default Button
