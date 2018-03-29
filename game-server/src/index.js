27aa83b1ddf8caf3a1bed70caff2f34345935de0
Mirko Geissler <mirko.geissler@artcom.de>
2018-03-29 15:09:08 +0200

    Open server after mongodb operations

--- game-server/src/index.js
+++ game-server/src/index.js
@@ -1,25 +1,24 @@
 const bunyan = require("bunyan")
-
-const CONNECTION_CONFIG = { pingInterval: 5000, pingTimeout: 2000 }
-const io = require("socket.io")(3000, CONNECTION_CONFIG)
+const io = require("socket.io")
 
 const Client = require("./client")
 const MongoDB = require("./api/mongoDB")
 
+const CONNECTION_CONFIG = { pingInterval: 5000, pingTimeout: 2000 }
 const LOG = bunyan.createLogger({ name: "game-server" })
 
 const mongoDB = new MongoDB(LOG)
-
 mongoDB.connect().then(async () => {
   LOG.info("Connected to MongoDB")
 
   await mongoDB.disconnectGames()
-
-  io.on("connect", socket => {
+  
+  // forward database update event to all clients
+  mongoDB.on("connectedGames", (connectedGames) => io.emit("connectedGames", connectedGames))
+  
+  const server = io(3000, CONNECTION_CONFIG)
+  server.on("connect", socket => {
     const client = new Client(socket, mongoDB, LOG)
     client.subscribe()
   })
-
-  // forward database update event to all clients
-  mongoDB.on("connectedGames", (connectedGames) => io.emit("connectedGames", connectedGames))
 }).catch((error) => LOG.error(error))
