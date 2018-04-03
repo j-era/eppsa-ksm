39c11b889e6d3ee95674fb77e079a3160e2e7212
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-04-03 13:05:39 +0200

    Extract theme

--- game-challenge/quiz/src/components/nextButton.js
+++ game-challenge/quiz/src/components/nextButton.js
@@ -1,8 +1,21 @@
 import styled from "styled-components"
+import React from "react"
 import Button from "./button"
+import ButtonIcon from "../assets/svg/EPPSA_Assets_Button_Icon.svg"
 
 
-export default styled(Button)`
+const NextButton = styled(Button)`
   margin-top: 2em;
+  height: 2em;
   width: 67%;
+  fill: black;
 `
+
+const NextIcon = styled(ButtonIcon)`
+  height: 50%;
+  fill: black;
+`
+
+export default props => {
+  return <NextButton>{ props.text }<NextIcon/></NextButton>
+}
---
+++ game-challenge/quiz/src/theme.js
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    primary: "#f5a159",
+    secondary: "#e5e5e5",
+    primaryFont: "#000000",
+    secondaryFont: "#7b7b7b",
+    areaColor: "#e05633",
+    rightAnswer: "#00d700",
+    wrongAnswer: "#f3352f"
+  },
+  layout: {
+    offsetX: "5vh",
+    borderRadius: "15px",
+    buttonBorder: "5px",
+  },
+  font: {
+    headline: { size: "4vh", weight: "bold", color: "#000000" },
+    button: { size: "3vh", weight: "normal", color: "#000000" },
+    text: { size: "2vh", weight: "normal", color: "#777777" }
+  }
+}
