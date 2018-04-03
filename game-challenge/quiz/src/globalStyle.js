b14f90e46e76011ae89b1bd35f920c596cb05c7d
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-04-03 10:15:17 +0200

    Extract injectGlobalStyle

---
+++ game-challenge/quiz/src/globalStyle.js
@@ -0,0 +1,68 @@
+import {injectGlobal} from "styled-components"
+
+
+const fontFamily = "Cabin"
+
+export function injectGlobalStyle(fontServer) {
+  injectGlobal`
+  body::-webkit-scrollbar {
+    display: none;
+  }
+
+  @font-face {
+    font-family: ${fontFamily};
+    font-weight: 400;
+    font-style: normal;
+    src: url("${fontServer}/Cabin/Cabin-Regular.ttf");
+  }
+  
+  @font-face {
+    font-family: ${fontFamily};
+    font-weight: 400;
+    font-style: italic;
+    src: url("${fontServer}/Cabin/Cabin-Italic.ttf");
+  }
+  
+  @font-face {
+    font-family: ${fontFamily};
+    font-weight: 500;
+    font-style: normal;
+    src: url("${fontServer}/Cabin/Cabin-Medium.ttf");
+  }
+  
+  @font-face {
+    font-family: ${fontFamily};
+    font-weight: 500;
+    font-style: italic;
+    src: url("${fontServer}/Cabin/Cabin-MediumItalic.ttf");
+  }
+  
+  @font-face {
+    font-family: ${fontFamily};
+    font-weight: 600;
+    font-style: normal;
+    src: url("${fontServer}/Cabin/Cabin-SemiBold.ttf");
+  }
+  
+  @font-face {
+    font-family: ${fontFamily};
+    font-weight: 600;
+    font-style: italic;
+    src: url("${fontServer}/Cabin/Cabin-SemiBoldItalic.ttf");
+  }
+  
+  @font-face {
+    font-family: ${fontFamily};
+    font-weight: 700;
+    font-style: normal;
+    src: url("${fontServer}/Cabin/Cabin-Bold.ttf");
+  }
+  
+  @font-face {
+    font-family: ${fontFamily};
+    font-weight: 700;
+    font-style: italic;
+    src: url("${fontServer}/Cabin/Cabin-BoldItalic.ttf");
+  }
+`
+}
--- game-challenge/quiz/src/index.js
+++ game-challenge/quiz/src/index.js
@@ -1,35 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { injectGlobal } from "styled-components"
+import { injectGlobalStyle } from "./globalStyle"
 import App from "./App"
 import "./index.css"
 import selectContent from "./selectContent"
 
+
+const FONT_URI = process.env.FONT_URI
+
+injectGlobalStyle(FONT_URI)
 
 window.addEventListener("message", receiveMessage, false)
 
 function receiveMessage(event) {
   console.log(event)
   const content = selectContent(event.data)
-  console.log(content.scoreCalculation)
   ReactDOM.render(
     <App content={ content } completeChallenge={ score =>
       event.source.postMessage({ source: "challenge", score }, event.origin) } />,
     document.getElementById("root")
   )
 }
-const fontFamily = "Cabin"
-const FONT_URI = process.env.FONT_URI
 
-injectGlobal`
-  body::-webkit-scrollbar {
-    display: none;
-  }
-
-  @font-face {
-    font-family: ${fontFamily};
-    font-weight: 100;
-    font-style: normal;
-    src: url("${FONT_URI}/Cabin/Cabin-Regular.ttf");
-  }
-`
