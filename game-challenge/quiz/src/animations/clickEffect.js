645be4223697932db85ff9c95da92b7f2e74c2a9
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-04-06 14:41:00 +0200

    Add transparency to cick effect

--- game-challenge/quiz/src/animations/clickEffect.js
+++ game-challenge/quiz/src/animations/clickEffect.js
@@ -2,9 +2,18 @@ import { css, keyframes } from "styled-components"
 
 export default function clickEffect() {
   const click = keyframes`
-    0% { transform: scale(1, 1); }
-    50%   { transform: scale(0.9, 0.9); }
-    100% { transform: scale(1, 1); }
+    0% {
+      transform: scale(1, 1);
+      opacity: 1;
+    }
+    50%   {
+      transform: scale(0.9, 0.9);
+      opacity: 0.5;
+    }
+    100% {
+      transform: scale(1, 1);
+      opacity: 1;
+    }
   `
   return css`animation: ${click} 100ms ease 0ms 1;`
 }
