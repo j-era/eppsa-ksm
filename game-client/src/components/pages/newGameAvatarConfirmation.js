edd2872149911687918e7a113a546aa83406cdd5
Mirko Geissler <mirko.geissler@artcom.de>
2018-04-11 22:10:18 +0200

    Implement newGameAvatarSelection/Confirmation

--- game-client/src/components/pages/newGameAvatarConfirmation.js
+++ game-client/src/components/pages/newGameAvatarConfirmation.js
@@ -1,5 +1,20 @@
 import React from "react"
 
-export default function NewGameAvatarConfirmation() {
-  return <div>NewGameAvatarConfirmation</div>
+import { updateGameState } from "../../actionCreators"
+import { NEW_GAME_AVATAR_SELECTION, NEW_GAME_NAME_SELECTION } from "../../gameStates"
+
+export default function NewGameAvatarConfirmation(props) {
+  const { assetServerUri, avatar, content, dispatch } = props
+
+  return (
+    <div>
+      <img src={ `${assetServerUri}/${content.avatars[avatar].icon.src}` } />
+      <button onClick={ () => dispatch(updateGameState(NEW_GAME_NAME_SELECTION)) }>
+        Zum Spielernamen
+      </button>
+      <button onClick={ () => dispatch(updateGameState(NEW_GAME_AVATAR_SELECTION)) }>
+        Zurück
+      </button>
+    </div>
+  )
 }
--- game-client/src/components/pages/newGameAvatarSelection.js
+++ game-client/src/components/pages/newGameAvatarSelection.js
@@ -1,5 +1,28 @@
 import React from "react"
 
-export default function NewGameAvatarSelection() {
-  return <div>NewGameAvatarSelection</div>
+import { updateAvatar, updateGameState } from "../../actionCreators"
+import { NEW_GAME_AVATAR_CONFIRMATION } from "../../gameStates"
+
+export default function NewGameAvatarSelection(props) {
+  const { content, dispatch } = props
+
+  return (
+    <div>
+      <div>Wähle einen Avatar</div>
+      { Object.keys(content.avatars).map(avatar => renderAvatarSelector(avatar, dispatch)) }
+    </div>
+  )
+}
+
+function renderAvatarSelector(avatar, dispatch) {
+  return (
+    <button
+      key={ avatar }
+      onClick={ () => {
+        dispatch(updateAvatar(avatar))
+        dispatch(updateGameState(NEW_GAME_AVATAR_CONFIRMATION))
+      } }>
+      { avatar }
+    </button>
+  )
 }
