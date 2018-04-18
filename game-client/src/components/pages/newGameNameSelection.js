76a4d8d1106e2e411ce3c67d178bd52bde685d39
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-04-18 10:04:35 +0200

    Style page

--- game-client/src/components/pages/newGameNameSelection.js
+++ game-client/src/components/pages/newGameNameSelection.js
@@ -1,20 +1,56 @@
 import React from "react"
+import styled from "styled-components"
 
 import { updateName, startNewGame } from "../../actionCreators"
+
+import FramedIcon from "../../../node_modules/eppsa-ksm-shared/styled-components/components/framedIcon"
+import Button from "../../../node_modules/eppsa-ksm-shared/styled-components/components/button"
+import NextButton from "../../../node_modules/eppsa-ksm-shared/styled-components/components/nextButton"
+
+
+const Container = styled.div `
+  font-family: ${props => props.theme.font.fontFamily};
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: ${props => props.theme.layout.offsetX};
+  padding-right: ${props => props.theme.layout.offsetX};
+  height: 100%;
+  width: 100%;
+`
+
+const ConfirmButton = styled(NextButton)`
+  border-color: ${props => props.theme.colors.secondary};
+`
+
+const NameInputContainer = styled(Button)`
+  padding-left: 2em;
+  padding-right: 2em;
+`
+
+const NameInput = styled.input`
+  width: 100%;
+  text-align: center;
+`
 
 export default function NewGameNameSelection(props) {
   const { assetServerUri, avatar, content, dispatch, gameServer, name, maxChallenges } = props
 
   return (
-    <div>
-      <img src={ `${assetServerUri}/${content.avatars[avatar].icon.src}` } />
-      <input
-        type="text"
-        value={ name }
-        onChange={ event => dispatch(updateName(event.target.value)) } />
-      <button onClick={ () => dispatch(startNewGame(name, avatar, maxChallenges, gameServer)) }>
-        Bestätigen
-      </button>
-    </div>
+    <Container>
+      <FramedIcon icon={ `${assetServerUri}/${content.avatars[avatar].icon.src}` } />
+      <NameInputContainer>
+        <NameInput
+          type="text"
+          value={ name }
+          placeholder="Spielername eingeben"
+          onChange={ event => dispatch(updateName(event.target.value)) } />
+      </NameInputContainer>
+      <ConfirmButton
+        visible
+        onClick={ () => dispatch(startNewGame(name, avatar, maxChallenges, gameServer)) }
+        text="Bestätigen" />
+    </Container>
   )
 }
