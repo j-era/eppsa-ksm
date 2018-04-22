c3a97644104b3785f75472f0282019adb71b303b
Mirko Geissler <mirko.geissler@artcom.de>
2018-04-22 22:18:15 +0200

    Layout gameManual

--- game-client/src/components/gameManual.js
+++ game-client/src/components/gameManual.js
@@ -1,16 +1,17 @@
 import React from "react"
+import { NextButton, PageTitle, StyledMarkdown } from "eppsa-ksm-shared"
 
 import { showGameManual } from "../actionCreators"
 
 export default function gameManual(props) {
-  const text = props.content.manualText
-
   return (
     <div>
-      <div>{ text }</div>
-      <button onClick={ () => props.dispatch(showGameManual(false)) }>
-        Ok
-      </button>
+      <PageTitle text={ props.content.gameManualTitle } />
+      <StyledMarkdown>{ props.content.gameManualText }</StyledMarkdown>
+      <NextButton
+        visible
+        onClick={ () => props.dispatch(showGameManual(false)) }
+        text="Ok" />
     </div>
   )
 }
