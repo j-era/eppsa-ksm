8596113f29b611b91f8faeeef3b88d08a96b0d51
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-04-26 17:12:44 +0200

    Add page elements

--- game-client/src/components/pages/navigationToNextArea.js
+++ game-client/src/components/pages/navigationToNextArea.js
@@ -1,15 +1,40 @@
 import React from "react"
+import styled, { withTheme } from "styled-components"
+import { Description, FramedIcon, NextButton, Page, PageTitle } from "eppsa-ksm-shared"
 
 import { updateGameState } from "../../actionCreators"
 import { QR_READER } from "../../gameStates"
 
-export default function NavigationToNextArea(props) {
-  const text = props.content.challenges[props.challengeNumber].navigation
+const Container = styled(Page)`
+  display: flex;
+  flex-direction: column;
+`
+
+const Content = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+`
+
+function NavigationToNextArea(props) {
+  const title = props.content.challenges[props.challengeNumber].description
 
   return (
-    <div>
-      <div>{ text }</div>
-      <button onClick={ () => props.dispatch(updateGameState(QR_READER)) }>QR-Code Scannen</button>
-    </div>
+    <Container>
+      <PageTitle text={ title } />
+      <Content>
+        <FramedIcon color={ props.theme.colors.area } />
+        <Description>
+        Gehe zum nächsten Bereich und scanne den QR-Code
+        </Description>
+        <NextButton
+          visible
+          onClick={ () => props.dispatch(updateGameState(QR_READER)) }
+          text="QR-Code Scannen" />
+      </Content>
+    </Container>
   )
 }
+
+export default withTheme(NavigationToNextArea)
