71f02fda5f5a016dd6de8c4779b7fad5feb7197b
GoodEvil2 <goodevil2@thegoodevil.com>
2018-04-26 11:24:13 +0200

    changed pivot img hidden

--- game-challenge/hidden/src/gameScene.js
+++ game-challenge/hidden/src/gameScene.js
@@ -81,7 +81,7 @@ class gameScene extends Phaser.Scene {
 
 	for(var blockElements in this.blockImages){
 		if(this.blockImages[blockElements].direction == ""){
-			var blocking = this.add.image(this.blockImages[blockElements].positionX,this.blockImages[blockElements].positionY, this.blockImages[blockElements].img).setOrigin(0,this.blockImages[blockElements].displayHeight).setName(imageKey).setInteractive();
+			var blocking = this.add.image(this.blockImages[blockElements].positionX,this.blockImages[blockElements].positionY, this.blockImages[blockElements].img).setOrigin(0,1).setName(imageKey).setInteractive();
 			if(blocking.displayWidth > this.blockMaxWidth){
 				var scale = (this.blockMaxWidth / blocking.displayWidth)
 				blocking.setScale(scale,scale);
@@ -114,7 +114,7 @@ class gameScene extends Phaser.Scene {
 	this.loadedImages = {};
 
 	for(var imageKey in this.imageArray){
-		var image = this.add.image(0,0, this.imageArray[imageKey].img).setOrigin(0,this.imageArray[imageKey].displayHeight).setInteractive().setName(imageKey);
+		var image = this.add.image(0,0, this.imageArray[imageKey].img).setOrigin(0,1).setInteractive().setName(imageKey);
 		this.loadedImages[imageKey] = image;
 	}
 
