0c743e25765eb5bfdb38aecf13699beea3b9dd0b
Mirko Geissler <mirko.geissler@artcom.de>
2018-04-30 14:48:20 +0200

    Layout challenge selection

--- game-client/src/components/pages/challengeSelection.js
+++ game-client/src/components/pages/challengeSelection.js
@@ -1,44 +1,94 @@
-import React from "react"
 import omit from "lodash.omit"
+import React from "react"
+import styled, { withTheme } from "styled-components"
+import {
+  Button,
+  Description,
+  NextButton,
+  FramedIcon,
+  Page,
+  PageTitle
+} from "eppsa-ksm-shared"
 
 import { selectChallengeType } from "../../actionCreators"
-import { CHALLENGE_MANUAL } from "../../gameStates"
 
-export default class ChallengeSelection extends React.Component {
+const SELECTION_TIMEOUT = 3000
+
+const Container = styled(Page)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+`
+const StyledNextButton = styled(NextButton)`
+  opacity: 0.3;
+`
+
+class ChallengeSelection extends React.Component {
   constructor(props) {
     super(props)
-    // this.state = { showResult: false }
-    // this.timeout = setTimeout(() => this.setState({ showResult: true }), 2000);
-    
-    // this.props.dispatch(selectChallenge())
+
+    const { content, challengeNumber } = this.props
+    const challengeTypes = omit(content.challenges[challengeNumber].challengeTypes, "template")
+
+    this.state = {
+      names: Object.keys(challengeTypes),
+      developmentView: false
+    }
+  }
+
+  componentDidMount() {
+    this.timeout = setTimeout(() => {
+      const { names } = this.state
+      const index = Math.floor(Math.random() * names.length)
+      this.selectChallengeType(names[index])
+    }, SELECTION_TIMEOUT)
   }
 
   componentWillUnmount() {
-    // clearTimeout(this.timeout)
+    clearTimeout(this.timeout)
   }
 
   render() {
-    const { content, challengeNumber } = this.props
-    const challengeTypes = content.challenges[challengeNumber].challengeTypes
-    
+    const { assetServerUri, content } = this.props
+    const { developmentView, names } = this.state
+
     return (
-      <div>
-        <div>{ content.challengeSelectionText }</div>
+      <Container>
+        <PageTitle>{ content.shared.texts.challengeSelectionTitle }</PageTitle>
         {
-          Object.keys(omit(challengeTypes, "template")).map((name) => this.renderChallengeType(name))
+          developmentView ?
+            names.map((name) =>
+              <Button key={ name } onClick={ () => this.selectChallengeType(name) }>
+                { name }
+              </Button>
+            ) :
+            <FramedIcon
+              scale={ 0.78 }
+              color={ this.props.theme.colors.area }
+              iconSrc={
+                `${assetServerUri}/${content.shared.assets.challengeSelectionRandom.src}` } />
         }
-      </div>
+        <Description>{ content.shared.texts.challengeSelectionText }</Description>
+        <StyledNextButton
+          visible
+          onClick={ () => this.onClick() }
+          text={ content.shared.texts.next } />
+      </Container>
     )
   }
- 
-  renderChallengeType(name) {
+
+  onClick() {
+    if (process.env.NODE_ENV === "development") {
+      clearTimeout(this.timeout)
+      this.setState({ developmentView: true })
+    }
+  }
+
+  selectChallengeType(name) {
     const { content, dispatch, assetServerUri, gameServerUri, staticServerUri } = this.props
-    return (
-      <button key={ name } onClick={ () =>
-        dispatch(selectChallengeType(name, content, assetServerUri, gameServerUri, staticServerUri))}>
-        { name }
-      </button>
-    )
+    dispatch(selectChallengeType(name, content, assetServerUri, gameServerUri, staticServerUri))
   }
 }
 
+export default withTheme(ChallengeSelection)
