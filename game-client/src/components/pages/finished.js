85690915d988ce509a473cfde42888b2f4a5aaed
Marco <marco.mojica@hotmail.de>
2018-04-30 12:43:43 +0200

    Add finish page style components

--- game-client/src/components/pages/finished.js
+++ game-client/src/components/pages/finished.js
@@ -1,10 +1,30 @@
 import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  height: 100%;
+  width: 60vw;
+  margin: auto;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+const Text = styled.div`
+  text-align: center;
+`
+
+const Score = styled.div`
+  text-align: center;
+  margin-top: 5vw;
+`
 
 export default function Finished(props) {
   return (
-    <div>
-      <div>{ props.content.finishText }</div>
-      <div>Score: { props.score }</div>
-    </div>
+    <Container>
+      <Text>{ props.content.finishText }</Text>
+      <Score>{ props.score }</Score>
+    </Container>
   )
 }
