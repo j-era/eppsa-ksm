9059e73b31f6c028cc36d959c0c14fdc51e45a3a
Mirko Geissler <mirko.geissler@artcom.de>
2018-04-30 11:22:21 +0200

    Layout ChallengeModeSelection

--- game-client/src/components/pages/challengeModeSelection.js
+++ game-client/src/components/pages/challengeModeSelection.js
@@ -1,17 +1,90 @@
 import React from "react"
+import autoBind from "react-autobind"
+import styled, { withTheme } from "styled-components"
+
+import {
+  delay,
+  FramedIcon,
+  NextButton,
+  Page,
+  PageTitle
+} from "eppsa-ksm-shared"
 
 import { joinChallengeLobby, updateGameState } from "../../actionCreators"
 import { CHALLENGE_MANUAL } from "../../gameStates"
 
-export default function ChallengeModeSelection(props) {
-  return (
-    <div>
-      <button onClick={ () => props.dispatch(updateGameState(CHALLENGE_MANUAL)) }>
-        Alleine spielen
-      </button>
-      <button onClick={ () => props.dispatch(joinChallengeLobby(props.gameServer)) }>
-        Zu zweit spielen
-      </button>
-    </div>
-  )
+const Container = styled(Page)`
+  display: flex;
+  flex-direction: column;
+`
+
+const Content = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+`
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const MultiplayerButton = styled(NextButton)`
+  margin-top: ${props => props.theme.layout.mediumSpacing}vw;
+  width: 75%;
+`
+
+const SingleplayerButton = styled(NextButton)`
+  margin-top: ${props => props.theme.layout.mediumSpacing}vw;
+  width: 75%;
+`
+
+class ChallengeModeSelection extends React.Component {
+  constructor(props) {
+    super(props)
+    autoBind(this)
+    this.state = { multiplayerClicked: false, singleplayerClicked: false }
+  }
+
+  render() {
+    const { assetServerUri, content, theme } = this.props
+
+    return (
+      <Container>
+        <PageTitle>{ content.shared.texts.challengeModeSelectionTitle }</PageTitle>
+        <Content>
+          <FramedIcon
+            scale={0.78}
+            color={ theme.colors.area }
+            iconSrc={ `${assetServerUri}/${content.shared.assets.skillIcon.src}` } />
+          <Buttons>
+            <MultiplayerButton
+              visible
+              onClick={ this.onMultiplayer }
+              clicked={ this.state.multiplayerClicked }
+              text={ content.shared.texts.challengeModeSelectionMultiplayerButton } />
+            <SingleplayerButton
+              onClick={ this.onSingleplayer }
+              clicked={ this.state.singleplayerClicked }
+              text={ content.shared.texts.challengeModeSelectionSingleplayerButton } />
+          </Buttons>
+        </Content>
+      </Container>
+    )
+  }
+
+  async onMultiplayer() {
+    this.setState({ multiplayerClicked: true })
+    await delay(100)
+    this.props.dispatch(joinChallengeLobby(this.props.gameServer))
+  }
+
+  async onSingleplayer() {
+    this.setState({ singleplayerClicked: true })
+    await delay(100)
+    this.props.dispatch(updateGameState(CHALLENGE_MANUAL))
+  }
 }
+
+export default withTheme(ChallengeModeSelection)
