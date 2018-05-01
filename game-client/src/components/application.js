59188639465a0b01c077ba573e09f89de0fccc44
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-05-01 12:23:01 +0200

    Extract score component

--- game-client/src/components/application.js
+++ game-client/src/components/application.js
@@ -1,55 +1,19 @@
 import React from "react"
 import { connect } from "react-redux"
-import styled, { css, ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 import cloneDeep from "lodash.clonedeep"
 
 import Card from "./card"
 import { default as Background } from "./background"
 import Header from "./header"
 import pages from "./pages"
+import Score from "./score"
 
 const Container = styled.div`
   background-color: white;
   height: 100%;
   display: flex;
   flex-direction: column;
-`
-
-const ScoreContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
-const Score = styled.div`
-  margin-top: 0.5em;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: ${props => props.theme.font.button.size}vw;
-  background-color: ${props => props.theme.colors.area};
-  color: white;
-  padding-left: 1em;
-  padding-right: 1em;
-  height: 1.2em;
-  border-radius: ${props => props.theme.layout.borderRadius};
-  ${props => props.visible
-    ? css`
-      transform: scale(1, 1);
-      opacity: 1;
-      transition:
-        transform 150ms cubic-bezier(0.2, 0.7, 0.55, 1.2) 500ms,
-        opacity  150ms ease 500ms;
-    `
-    : css`
-      transform: scale(0, 0);
-      opacity: 0;
-      transition:
-        transform 150ms cubic-bezier(0.2, 0.7, 0.55, 1.2),
-        opacity  150ms ease;
-    `}
 `
 
 function Application(props) {
@@ -62,13 +26,7 @@ function Application(props) {
       theme={ (theme) => updateTheme(theme, challenge) }>
       <Container>
         { showHeader && <Header props={ props } /> }
-        {
-          <ScoreContainer>
-            <Score visible={ props.showScore }>
-              { props.challengeScore }
-            </Score>
-          </ScoreContainer>
-        }
+        <Score visible={ props.showScore } text={ props.challengeScore } />
         <Background
           { ...props }
           bannerText={ content.name } >
---
+++ game-client/src/components/score.js
@@ -0,0 +1,44 @@
+import React from "react"
+import styled, { css } from "styled-components"
+
+const ScoreContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Score = styled.div`
+  margin-top: 0.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${props => props.theme.font.button.size}vw;
+  background-color: ${props => props.theme.colors.area};
+  color: white;
+  padding-left: 1em;
+  padding-right: 1em;
+  height: 1.2em;
+  border-radius: ${props => props.theme.layout.borderRadius};
+  ${props => props.visible
+    ? css`
+      transform: scale(1, 1);
+      opacity: 1;
+      transition:
+        transform 150ms cubic-bezier(0.2, 0.7, 0.55, 1.2) 500ms,
+        opacity  150ms ease 500ms;
+    `
+    : css`
+      transform: scale(0, 0);
+      opacity: 0;
+      transition:
+        transform 150ms cubic-bezier(0.2, 0.7, 0.55, 1.2),
+        opacity  150ms ease;
+    `}
+`
+
+export default props =>
+  <ScoreContainer>
+    <Score visible={ props.visible }>{ props.text }</Score>
+  </ScoreContainer>
