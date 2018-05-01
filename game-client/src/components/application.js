e0ccfcae7cc10f0adfbb5e5ef1d39611b11f355d
globz-eu <16118065+globz-eu@users.noreply.github.com>
2018-05-01 12:13:40 +0200

    Add score component and animations

--- game-client/src/components/application.js
+++ game-client/src/components/application.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { connect } from "react-redux"
-import styled, { ThemeProvider } from "styled-components"
+import styled, { css, ThemeProvider } from "styled-components"
 import cloneDeep from "lodash.clonedeep"
 
 import Card from "./card"
@@ -15,6 +15,43 @@ const Container = styled.div`
   flex-direction: column;
 `
 
+const ScoreContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Score = styled.div`
+  margin-top: 0.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${props => props.theme.font.button.size}vw;
+  background-color: ${props => props.theme.colors.area};
+  color: white;
+  padding-left: 1em;
+  padding-right: 1em;
+  height: 1.2em;
+  border-radius: ${props => props.theme.layout.borderRadius};
+  ${props => props.visible
+    ? css`
+      transform: scale(1, 1);
+      opacity: 1;
+      transition:
+        transform 150ms cubic-bezier(0.2, 0.7, 0.55, 1.2) 500ms,
+        opacity  150ms ease 500ms;
+    `
+    : css`
+      transform: scale(0, 0);
+      opacity: 0;
+      transition:
+        transform 150ms cubic-bezier(0.2, 0.7, 0.55, 1.2),
+        opacity  150ms ease;
+    `}
+`
+
 function Application(props) {
   const { content } = props
   const { render, showHeader } = getPageData(props)
@@ -25,7 +62,13 @@ function Application(props) {
       theme={ (theme) => updateTheme(theme, challenge) }>
       <Container>
         { showHeader && <Header props={ props } /> }
-        { props.showScore && <div>{ props.challengeScore }</div>}
+        {
+          <ScoreContainer>
+            <Score visible={ props.showScore }>
+              { props.challengeScore }
+            </Score>
+          </ScoreContainer>
+        }
         <Background
           { ...props }
           bannerText={ content.name } >
