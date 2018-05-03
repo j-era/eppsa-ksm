a0b742e984be485d0a576144f14c1cf2d0974b9a
Jens Eraßmy <jens.erassmy@artcom.de>
2018-05-03 09:57:37 +0200

    Adapt to changes in eppsa-ksm-shared

--- game-challenge/sorting/src/App.js
+++ game-challenge/sorting/src/App.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { ThemeProvider } from "styled-components"
-import { theme } from "eppsa-ksm-shared"
+import { calculateTheme } from "eppsa-ksm-shared"
 
 import SortingGame from "./sortingGame"
+
+const theme = calculateTheme()
 
 export default (props) =>
   <ThemeProvider theme={ theme }>
