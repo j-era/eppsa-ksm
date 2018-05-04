59a462c525bb0632e97a995c3fb1471fdb5e8a4f
Mirko Geissler <mirko.geissler@artcom.de>
2018-05-04 15:05:09 +0200

    Style finish screen

--- game-client/src/components/pages/finished.js
+++ game-client/src/components/pages/finished.js
@@ -1,30 +1,51 @@
 import React from "react"
 import styled from "styled-components"
+import { Button, PageTitle, Page, StyledMarkdown } from "eppsa-ksm-shared"
 
-const Container = styled.div`
-  height: 100%;
-  width: 60vw;
-  margin: auto;
-  
+const Container = styled(Page)`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
 `
 
-const Text = styled.div`
+const TopText = styled(StyledMarkdown)`
+  display:flex;
+  text-align: center;
+  flex-direction: column;  
+  color: ${props => props.theme.font.headline.color};
+  font-weight: ${props => props.theme.font.headline.weight};
+`
+
+const ScoreContainer = styled.div`
+  display:flex;
+  flex-direction: column;  
   text-align: center;
 `
 
-const Score = styled.div`
+const ScoreText = styled.div`
   text-align: center;
-  margin-top: 5vw;
+  font-size: ${props => props.theme.font.text.size}vw;
+  font-weight: ${props => props.theme.font.headline.weight};
+  color: ${props => props.theme.font.headline.color};
+  margin-bottom: 1em;
 `
 
-export default function Finished(props) {
+const BottomText = styled(StyledMarkdown)`
+  display:flex;
+  text-align: center;
+  flex-direction: column;
+`
+
+export default function Finished({ content, score }) {
   return (
     <Container>
-      <Text>{ props.content.finishText }</Text>
-      <Score>{ props.score }</Score>
+      <PageTitle>{ content.shared.texts.finishTitle }</PageTitle>
+      <TopText>{ content.shared.texts.finishTopText }</TopText>
+      <ScoreContainer>
+        <ScoreText>{ content.shared.texts.finishScoreText }</ScoreText>
+        <Button>{ score }</Button>
+      </ScoreContainer>
+      <BottomText>{ content.shared.texts.finishBottomText }</BottomText>
     </Container>
   )
 }
