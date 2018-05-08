28d0bc89c4ad13d792e1ed170ca25933c1863814
Marco <marco.mojica@hotmail.de>
2018-05-08 14:50:38 +0200

    Refactor

--- game-client/src/components/pages/challengeSelection.js
+++ game-client/src/components/pages/challengeSelection.js
@@ -29,33 +29,34 @@ class ChallengeSelection extends React.Component {
     super(props)
 
     const { content, challengeNumber } = this.props
+
     this.challengeTypes = omit(content.challenges[challengeNumber].challengeTypes, "template")
+    this.names = Object.keys(this.challengeTypes)
 
     this.state = {
-      names: Object.keys(this.challengeTypes),
       developmentView: false
     }
   }
 
   componentDidMount() {
-    const challanges = new Map()
+    const challangeMap = new Map()
 
-    this.state.names.forEach(challengeName => {
+    this.names.forEach(challengeName => {
       const challenge = this.challengeTypes[challengeName]
 
-      if (challanges.has(challenge.template)) {
-        challanges.get(challenge.template).push(challengeName)
+      if (challangeMap.has(challenge.template)) {
+        challangeMap.get(challenge.template).push(challengeName)
       } else {
-        challanges.set(challenge.template, [challengeName])
+        challangeMap.set(challenge.template, [challengeName])
       }
     })
 
-    const challengeTypes = Array.from(challanges.keys())
+    const challengeTypes = Array.from(challangeMap.keys())
 
     this.timeout = setTimeout(() => {
       const challengeType = challengeTypes[Math.floor(Math.random() * challengeTypes.length)]
-      const index = Math.floor(Math.random() * challanges.get(challengeType).length)
-      this.selectChallengeType(challanges.get(challengeType)[index])
+      const index = Math.floor(Math.random() * challangeMap.get(challengeType).length)
+      this.selectChallengeType(challangeMap.get(challengeType)[index])
     }, SELECTION_TIMEOUT)
   }
 
@@ -65,14 +66,14 @@ class ChallengeSelection extends React.Component {
 
   render() {
     const { assetServerUri, content } = this.props
-    const { developmentView, names } = this.state
+    const { developmentView } = this.state
 
     return (
       <Container>
         <PageTitle>{ content.shared.texts.challengeSelectionTitle }</PageTitle>
         {
           developmentView ?
-            names.map((name) =>
+            this.names.map((name) =>
               <Button key={ name } onClick={ () => this.selectChallengeType(name) }>
                 { name }
               </Button>
