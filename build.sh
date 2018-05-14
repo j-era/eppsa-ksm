0e64a1c4e1d255c11c9420657d9ee813a0dff065
Marco <marco.mojica@hotmail.de>
2018-05-14 16:09:25 +0200

    Add stats frontend to environment

--- build.sh
+++ build.sh
@@ -6,11 +6,13 @@ export ASSET_SERVER_URI=https://asset-server.${HOST}
 export GAME_SERVER_URI=https://game-server.${HOST}
 export FONT_URI=https://static.${HOST}/fonts
 export STATIC_SERVER_URI=https://static.${HOST}
+export MONGO_ACCESS_URI=https://mongo.${HOST}
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 declare -a SITES=(
   "dashboard"
   "game-client"
+  "game-stats"
   "game-challenge/clicker"
   "game-challenge/graph"
   "game-challenge/hidden"
