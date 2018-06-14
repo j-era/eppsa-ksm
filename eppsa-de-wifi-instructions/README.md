8c2cfabbb01ba282ce06bf115913357e992d6e48
Marco <marco.mojica@hotmail.de>
2018-06-14 15:46:44 +0200

    Add Text and ssl configuration

---
+++ eppsa-de-wifi-instructions/README.md
@@ -0,0 +1,11 @@
+# Wifi instructions
+
+Gives lost users instructions how to join the eppsa wifi.
+
+Provides SSID and Password
+
+# Run
+
+```
+CERT=<path/to/cert.pem> KEY=<path/to/key.pem> npm run start
+```
--- eppsa-de-wifi-instructions/src/App.js
+++ eppsa-de-wifi-instructions/src/App.js
@@ -1,7 +1,33 @@
+import React from "react"
 import styled from "styled-components"
 
-export default styled.div`
-  width: 100%;
+const Container = styled.div`
   height: 100%;
-  background-color: yellow;
+
+  display: flex;
+  
+  align-items: center;
+  justify-content: center;
+  
+  text-align: center;
+  
+  padding-left: 20%;
+  padding-right: 20%;
+
 `
+
+export default () =>
+  <Container>
+    UPS!!
+    <br />
+    <br />
+    <br />
+    <br />
+    Du hast den QR-Code gescanned aber dich nicht im WLAN angemeldet.
+    <br />
+    <br />
+    <br />
+    <br />
+    Verbinde dich mit den WLAN {"<SSID>"} dem dem Passwort
+    {"<PW>"}, um an der Schnitzeljagd teilzunehmen.
+  </Container>
