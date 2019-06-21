e46a935c830d466bcd7a1f288153302167d4500c
Marco <marco.mojica@artcom.de>
2019-06-21 12:43:13 +0200

    Remove unused challenges

--- build.sh
+++ build.sh
@@ -11,12 +11,9 @@ BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 declare -a SITES=(
   "game-client"
   "game-challenge/clicker"
-  "game-challenge/graph"
   "game-challenge/hidden"
   "game-challenge/quiz"
-  "game-challenge/skill"
   "game-challenge/sorting"
-  "game-challenge/astragal"
 )
 
 for i in "${SITES[@]}"
