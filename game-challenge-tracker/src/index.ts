3308714dd404bd368330d4e9ec07303ee9ae745a
Marco <marco.mojica@artcom.de>
2019-07-04 13:31:20 +0200

    Log data only if challengeCompletion is present

--- game-challenge-tracker/src/index.ts
+++ game-challenge-tracker/src/index.ts
@@ -5,9 +5,21 @@ import { IncomingMessage } from "http"
 
 const { appendFile } = fsPromises
 
+type ChallengeCompletion = {
+  gameID: string
+  challengeNumber: number
+  score: number
+  finished: boolean
+  time: string
+}
+
 async function handleRequest(request: IncomingMessage) {
-  const challengeCompletion = await json(request)
-  await appendFile("./logs/log.txt", `${JSON.stringify(challengeCompletion)}\n`)
+  const challengeCompletion = await json(request) as ChallengeCompletion
+
+  if (challengeCompletion) {
+    challengeCompletion.time = new Date().toLocaleString()
+    await appendFile("./logs/log.txt", `${JSON.stringify(challengeCompletion)}\n`)
+  }
   return ""
 }
 
